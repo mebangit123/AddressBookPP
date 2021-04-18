009aa55c4c16fddd86784c3fc11b90916b92720b
mebangit123 <mebannongrum333@gmail.com>
2021-04-18 17:37:28 +0530

    Add new Contact to AddressBook

--- AddressBook/src/Contacts.java
+++ AddressBook/src/Contacts.java
@@ -1,4 +1,3 @@
-
 public class Contacts {
 	private String f_name;
 	private String l_name;
@@ -8,20 +7,7 @@ public class Contacts {
 	private int zip;
 	private int phone_no;
 	private String email;
-	
-	
-	
-	public Contacts(String f_name, String l_name, String address, String city, String state, int zip, int phone_no,
-			String email) {
-		this.f_name = f_name;
-		this.l_name = l_name;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.zip = zip;
-		this.phone_no = phone_no;
-		this.email = email;
-	}
+
 	
 	public String getF_name() {
 		return f_name;
@@ -49,6 +35,32 @@ public class Contacts {
 		return email;
 	}
 	
+	
+	
+	public void setF_name(String f_name) {
+		this.f_name = f_name;
+	}
+	public void setL_name(String l_name) {
+		this.l_name = l_name;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public void setPhone_no(int phone_no) {
+		this.phone_no = phone_no;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	@Override
 	public String toString() {
 		return "Contacts [FirstName=" + f_name + " LastName=" + l_name + " Address=" + address + " City=" + city
